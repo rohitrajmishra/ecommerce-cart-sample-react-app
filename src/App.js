dab3a292bb823ae477dd24744a7a875f628a8d6b
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-12-20 20:44:59 +0530

    Add state for CartItem component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import CartItem from './CartItem';
 function App() {
   return (
     <div className="App">
-      <h1>Cart!</h1>
       <CartItem />
     </div>
   );
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 
 class CartItem extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
   }
 
   render() {
+    const { title, price, qty, img } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} src="" alt="" />
+          <img style={styles.image} src={img} alt="" />
         </div>
         <div className="right-block">
-          <div>Title</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs. {price}</div>
+          <div style={{ color: "#777" }}>Qty: {qty}</div>
 
-          <div style={{ color: "#777" }}>Price</div>
-
-          <div style={{ color: "#777" }}>Quantity</div>
-
-          <div className="cart-item-actions">Buttons</div>
+          <div className="cart-item-actions">
+            <img src="https://www.flaticon.com/svg/static/icons/svg/149/149145.svg" className="action-icons" alt="increase" />
+            <img src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" className="action-icons" alt="decrease" />
+            <img src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" className="action-icons" alt="delete" />
+          </div>
         </div>
       </div>
     );
