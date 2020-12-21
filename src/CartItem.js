3a79023c93b63ba36af284b4926b8c3595b8e00b
rohitrajmishra <rohitrajmishra.nits@gmail.com>
2020-12-21 19:06:50 +0530

    Setup increase qty logic with setState

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,6 +11,17 @@ class CartItem extends Component {
     };
   }
 
+  increaseQuantity = () => {
+    console.log("this.state", this.state);
+    // setState form2 with prevState
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1,
+      };
+    });
+  };
+
+  
   render() {
     const { title, price, qty, img } = this.state;
     return (
@@ -24,9 +35,22 @@ class CartItem extends Component {
           <div style={{ color: "#777" }}>Qty: {qty}</div>
 
           <div className="cart-item-actions">
-            <img src="https://www.flaticon.com/svg/static/icons/svg/149/149145.svg" className="action-icons" alt="increase" />
-            <img src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg" className="action-icons" alt="decrease" />
-            <img src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg" className="action-icons" alt="delete" />
+            <img
+              onClick={this.increaseQuantity}
+              src="https://www.flaticon.com/svg/static/icons/svg/149/149145.svg"
+              className="action-icons"
+              alt="increase"
+            />
+            <img
+              src="https://www.flaticon.com/svg/static/icons/svg/992/992683.svg"
+              className="action-icons"
+              alt="decrease"
+            />
+            <img
+              src="https://www.flaticon.com/svg/static/icons/svg/1214/1214428.svg"
+              className="action-icons"
+              alt="delete"
+            />
           </div>
         </div>
       </div>
